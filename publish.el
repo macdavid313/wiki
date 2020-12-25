95b1e07ae1f6d6057a1b73eae7b15de3f97c45d6
Tianyu Gu <macdavid313@gmail.com>
2020-12-25 19:40:31 +1100

    install org-roam from apt

--- publish.el
+++ publish.el
@@ -23,8 +23,8 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
-(dolist (pkg '(org-roam))
-  (package-install pkg))
+;; (dolist (pkg '(org-roam))
+;;   (package-install pkg))
 ;; -- End of set up
 
 (require 'ox-publish)
