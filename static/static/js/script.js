6a3201c1e9c78fda3177c1005109968a67f434dd
Tianyu Gu <macdavid313@gmail.com>
2020-12-27 17:45:50 +1100

    update script.js

--- static/static/js/script.js
+++ static/static/js/script.js
@@ -2,7 +2,7 @@
 var header = document.getElementsByTagName("header")[0];
 header.insertAdjacentHTML("afterend", "<p><nav class=\"title\">\n" +
     "<a href =\"https://macdavid313.xyz/wiki/\"><b>wiki</b></a>.&nbsp;\n" +
-    "<a href =\"https://macdavid313.xyz/wiki/full_index.html\"><b>full index</b></a>.&nbsp;\n" +
+    "<a href =\"https://macdavid313.xyz/wiki/full_index.html\"><b>index</b></a>.&nbsp;\n" +
     "<a href =\"https://macdavid313.xyz/wiki/recent_changes.html\"><b>recent changes</b></a>.&nbsp;\n" +
     "<a href =\"https://macdavid313.xyz/\"><b>home page</b></a>.&nbsp;\n" +
     "</nav ></p>\n");
